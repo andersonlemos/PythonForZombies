c0058341ff21d8aff9d5234e665a5799e05f6fe4
Anderson Lemos <alemos@openmailbox.org>
2015-02-10 23:32:26 -0200

    Update

--- Web/pythonWebBottle3.py
+++ Web/pythonWebBottle3.py
@@ -1,1 +1,17 @@
 __author__ = 'anderson'
+import bottle
+
+@bottle.route('/')
+def home_page():
+    myThings = ['Python', 'Ruby', 'Perl', 'C++']
+    return bottle.template('./templates/hello_world', {'username': 'Masanori',
+                                                       'things': myThings})
+@bottle.post('/favorita',)
+def favorita():
+    lang = bottle.request.forms.get('lang')
+    if (lang == None or lang == ''):
+        lang = 'Nenhuma Escolhida'
+    return bottle.template('./templates/lang_selection', {'lang': lang})
+
+bottle.debug(True)
+bottle.run(host='localhost', port=8080)
