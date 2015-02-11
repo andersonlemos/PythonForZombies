46ba7a500961c60be4536c369db731781c82701d
Anderson Lemos <alemos@openmailbox.org>
2015-02-11 00:20:59 -0200

    Update

--- Web/pythonWebBottle3.py
+++ Web/pythonWebBottle3.py
@@ -1,17 +1,17 @@
 __author__ = 'anderson'
 import bottle
-
+templates_path='./templates/'
 @bottle.route('/')
 def home_page():
-    myThings = ['Python', 'Ruby', 'Perl', 'C++']
-    return bottle.template('./templates/hello_world', {'username': 'Masanori',
-                                                       'things': myThings})
-@bottle.post('/favorita',)
+    myThings = ['Python', 'Ruby', 'Perl', 'C++', 'Outra']
+    return bottle.template(templates_path + 'hello_world', {'username': 'Masanori',
+                                                       'things' : myThings})
+@bottle.post('/favorita')
 def favorita():
     lang = bottle.request.forms.get('lang')
     if (lang == None or lang == ''):
         lang = 'Nenhuma Escolhida'
-    return bottle.template('./templates/lang_selection', {'lang': lang})
+    return bottle.template(templates_path + 'lang_selection', {'lang': lang})
 
 bottle.debug(True)
 bottle.run(host='localhost', port=8080)
